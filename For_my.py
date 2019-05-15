37dee72b03ef3777768170bf3dd81d4a060b9e55
Roberto <rob.uniud@gmail.com>
2019-05-15 17:41:57 +0200

    Readability improvement

--- For_my.py
+++ For_my.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+# Created by Roberto Cella
+# For any question mail me at rob.uniuc@gmail.com
 
 """
-Scrivere uscite ogni settimana, con cronologia
-Scrivo i litri di latte usato e moltiplica * 0.4 e aggiungo alle spese
+Scrivere le uscite ogni settimana, con cronologia
+Scrivere i litri di latte usati e moltiplicarli * 0.4, aggiungendoli alle spese
 Tenere conto dei litri (quanti litri ho usato finora?)
+Vedere le entrate al netto delle spese
 Grafico:
-	x = tempo, y = litri lavorati
 	x = tempo, y = soldi guadagnati
-	- sovrapposizione
+    x = tempo, y = litri lavorati
+	- sovrapposizione dei due grafici
 """
 import sys
 import csv
@@ -47,7 +50,8 @@ Benvenuto in For_my, scegli un'opzione:
                     writer.writerow([f"{datetime.datetime.now():%d-%m-%Y}", input(
                         "Quanto hai speso questa settimana? "), input(
                         "Quanti litri di latte hai lavorato? "), input(
-                        "Quanto hai guadagnato questa settimana? ")])
+                        "Quanto hai guadagnato questa settimana? ")
+                    ])
             elif choice == "2":  # cronologia spese
                 print(f"\nLe spese totali sono state di {summer(1)}€.")
             elif choice == "3":  # cronologia latte
@@ -57,22 +61,26 @@ Benvenuto in For_my, scegli un'opzione:
                 print(f"\nHai guadagnato un netto di {gain}0€")
             elif choice == "5":  # grafico
                 df = pd.read_csv("data.csv")
+
                 trace_high = go.Scatter(
                     x=df.Date,
                     y=df["Guadagno"],
                     name="Soldi guadagnati",
                     line=dict(color='#57E53D'),
                     opacity=0.8)
+
                 trace_low = go.Scatter(
                     x=df.Date,
                     y=df["Latte usato"],
                     name="Litri di latte",
                     line=dict(color='#17BECF'),
                     opacity=0.8)
+
                 plotly.offline.plot({
                     "data": [trace_high, trace_low],
                     "layout": go.Layout(title="Guadagni e litri usati:")
                 }, auto_open=True, filename='graph.html')
+
             elif choice == "0":  # chiusura
                 keyboard.press(Key.alt)
                 keyboard.press(Key.f4)
